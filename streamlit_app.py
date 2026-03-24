c17c3c3f9186fd55893262b4e1f90f2b2f017c2d
Ameer <159410159+ameeralex@users.noreply.github.com>
2026-03-24 21:42:27 +0530

    Update streamlit_app.py
    Updated the import statement and modified the table reference for fruit options. Converted the Snowflake DataFrame to a pandas DataFrame for further processing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
  
 # Write directly to the app
@@ -21,15 +20,17 @@ st.write('The name on your smoothie will be:',name_on_order)
  
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select("FRUIT_NAME")
+
 df = my_dataframe.to_pandas()
-fruit_list = df['FRUIT_NAME'].tolist()
+fruit_list = df["FRUIT_NAME"].tolist()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     fruit_list,
 )
+
 if(ingredients_list):
     # st.write(ingredients_list)
     # st.text(ingredients_list)
